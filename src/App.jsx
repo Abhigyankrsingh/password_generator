8633d81eb3dc57d9c29d16dc65d6ee1e4b72e26c
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 17:25:26 +0530

    Code_update

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,9 @@ function App() {
   }, [length, numberAllowed, charAllowed, setPassword])
   return (
     <>
-     <h1 className='text-4xl text-center text-white'>Password Generator</h1> 
+     
+
+    <div></div>
     </>
   )
 }
