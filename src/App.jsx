617dbf12706ba9d27a8fdce5db76da42eca824ee
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 16:37:54 +0530

    Code_Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   const[length, setLength] = useState(8)
   const[numberAllowed, setNumberAllowed] = useState(false);
   const[charAllowed,setcharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
+
+  const passwordGenerator 
   return (
     <>
      <h1 className='text-4xl text-center text-white'>Password Generator</h1> 
